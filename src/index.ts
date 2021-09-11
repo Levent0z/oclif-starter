609c1da820c65a101a0cfc045a184310bc59868a
Levent Oz <leventoz.dev@gmail.com>
2021-09-10 17:09:44 -0700

    Add demos

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,80 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command'
 
 class OclifStarter extends Command {
-  static description = 'describe the command here'
+  static description = 'A template command to demonstrate OCLIF usage.';
 
+  // https://oclif.io/docs/args
+  // static args = [{ name: 'file' }]; // Positional arguments
+  // static strict = false; // For variable number of arguments
+
+  // https://oclif.io/docs/flags
   static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
+    version: flags.version(),
+    help: flags.help({ char: 'h' }),
+    single: flags.string({
+      char: 's',
+      description: 'A single argument'
+    }),
+    multi: flags.string({
+      char: 'm',
+      description: 'An argument that can be repeated',
+      multiple: true
+    }),
+    dependent: flags.string({
+      char: 'd',
+      description: "An arg that's dependent on the single argument and excludes the multi argument",
+      dependsOn: ['single'],
+      exclusive: ['multi']
+    }),
+    'env-arg': flags.string({
+      char: 'e',
+      description: 'An argument that will override the value provided in the env variable "MY_CLI_ENV_ARG".',
+      env: 'MY_CLI_ENV_ARG'
+    }),
+    boolean: flags.boolean({
+      description: 'A boolean argument'
+    }),
+    error: flags.boolean({
+      description: 'A boolean argument that logs an error'
+    })
+  };
 
   async run() {
-    const {args, flags} = this.parse(OclifStarter)
+    const { args, flags } = this.parse(OclifStarter)
+    let hasArgs = false;
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    if (flags.single) {
+      this.log(`Single arg has value ${flags.single}`);
+      hasArgs = true;
+    }
+
+    const multi: string[] = flags.multi;
+    if (multi) {
+      this.log(`Multi arg has values [${multi.join(', ')}]`);
+      hasArgs = true;
+    }
+
+    if (flags.dependent) {
+      this.log(`Dependent arg has value ${flags.dependent}`);
+      hasArgs = true;
+    }
+
+    if (flags['env-arg']) {
+      this.log(`"env-arg" arg has value ${flags['env-arg']}`);
+      hasArgs = true;
+    }
+
+    if (flags['boolean']) {
+      this.log(`The boolean has value ${flags['boolean']}`);
+      hasArgs = true;
+    }
+
+    if (flags['error']) {
+      this.error(`The error arg was used. Exiting.`, { exit: 1 });
+    }
+
+    if (!hasArgs) {
+      await OclifStarter.run(['--help']);
     }
   }
 }
